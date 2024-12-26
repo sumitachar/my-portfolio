b680dd28f028364f291d2023684f0d3e7245efa2
SUMIT ACHAR <sumitachar@SUMITs-MacBook-Air.local>
2024-12-26 23:06:04 +0530

    seventh commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,6 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
